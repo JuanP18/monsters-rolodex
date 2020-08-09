c90a1146fc6c6ee53cd84c24639a1a08c64755a1
Juan Pablo <jp_1805@hotmail.com>
2020-08-09 14:08:55 -0500

    New component seach and a few improvements

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React,{Component}  from 'react';
 import './App.css';
 import { CardList } from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
 
 class App extends  Component{
     constructor() {
         super();
         this.state = {
-            monsters: []
+            monsters: [],
+            searhField: ''
         }
     }
     componentDidMount(){
@@ -15,9 +17,12 @@ class App extends  Component{
          .then(user => this.setState({monsters: user}));
     }
     render() {
+        const { monsters, searhField } = this.state;
+        const filterMonster = monsters.filter( monster => monster.name.toLowerCase().includes(searhField.toLowerCase()));
         return (
             <div className="App">
-            <CardList monsters={this.state.monsters} />
+            <SearchBox placeHolder="search monsters" handleChange={ (event) => this.setState({searhField: event.target.value}) } />
+            <CardList monsters={filterMonster} />
             </div>
         );
     }
