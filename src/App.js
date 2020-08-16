434adfab5f9459ee134aa23de253243da26f3485
Juan Pablo <jp_1805@hotmail.com>
2020-08-15 21:05:36 -0500

    Some Changes

--- src/App.js
+++ src/App.js
@@ -10,18 +10,26 @@ class App extends  Component{
             monsters: [],
             searhField: ''
         }
+        
+        // this.handleChange = this.handleChange.bind(this);
     }
     componentDidMount(){
         fetch('https://jsonplaceholder.typicode.com/users')
          .then( response => response.json())
          .then(user => this.setState({monsters: user}));
     }
+
+    handleChange = (event) => {
+        this.setState({searhField: event.target.value});
+    }
+
     render() {
         const { monsters, searhField } = this.state;
         const filterMonster = monsters.filter( monster => monster.name.toLowerCase().includes(searhField.toLowerCase()));
         return (
             <div className="App">
-            <SearchBox placeHolder="search monsters" handleChange={ (event) => this.setState({searhField: event.target.value}) } />
+            <h1>Monters Rolodex</h1>
+            <SearchBox placeHolder="search monsters" handleChange={this.handleChange } />
             <CardList monsters={filterMonster} />
             </div>
         );
