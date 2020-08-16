55f09af7659b9bf618c86cc35d7728c781e2ff3a
Juan Pablo <jp_1805@hotmail.com>
2020-08-15 21:41:16 -0500

    Updates

---
+++ precache-manifest.996376872d1e13a226060b76922147f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc28ecc928f034a7ec0d6e07a3d3092b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "526d2ccc5451e2af7d66",
+    "url": "/monsters-rolodex/static/css/main.184ba630.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "526d2ccc5451e2af7d66",
+    "url": "/monsters-rolodex/static/js/main.6d876ca6.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
